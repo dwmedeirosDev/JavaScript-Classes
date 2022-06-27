05160d098fd6d82ce7c0172ed0966ef6a6f7978e
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-27 14:05:17 -0300

    Atualizado

---
+++ Aula 7 JS - Dom/script.js
@@ -0,0 +1,56 @@
+// // ##### AULA 7 27.06.22 #####
+
+// // Crie um array que receba 5 itens e exiba no console.
+
+// let tecnologias = ["JavaScript", "CSS", "HTML", "React", "TypeScript"];
+// console.log("Array que exiga as tecnologias", tecnologias)
+
+// // Utilize um método para adicionar um nome ao inicio do array.
+
+// tecnologias.unshift("NextJS")
+// console.log("Adicionando uma tecnologia no início", tecnologias)
+
+// // Utilize um método para remover o último nome do array.
+
+// tecnologias.pop()
+// console.log("Removendo a ultima tecnologia", tecnologias)
+
+// // Utilize um método para adicionar dois nomes ao fim do array.
+
+// tecnologias.push("SasS", "VueJS")
+// console.log("Adionando ao final 2 tecnologias", tecnologias)
+
+// // Utilize um método para remover o primeiro nome do array.
+
+// tecnologias.shift()
+// console.log("Removendo a primeira tecnologia:", tecnologias)
+
+// // Utilize um método para organizar em ordem crescente o seguinte array:
+
+// let number = [2, 9, 8, 4, 5]
+// console.log(number)
+// console.log("Número em ordem numérica:", number.sort());
+
+// ############################################################################################
+
+// Manipulação do DOM 
+// Document Object Model
+
+// console.log(window.alert("Bem Vindo(a)"))
+
+// console.log(window.prompt("Qual seu nome?"))
+
+// window.alert("Seja bem-vindo(a)");
+
+// let nome = window.prompt("Qual é o seu nome?");
+
+// window.alert(`Bem vindo(a), ${nome}`);
+
+console.log("Aqui é o window", window);
+console.log("Aqui é o document", document);
+
+document.body.style.backgroundColor = "purple";
+
+
+
+
